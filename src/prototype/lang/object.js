6e8032899d570fe81475ed00983c7440f883c76c
Victor <vkhomyackov@gmail.com>
2013-10-07 21:05:34 +0300

    `Object.create` polyfill

--- src/prototype/lang/object.js
+++ src/prototype/lang/object.js
@@ -605,6 +605,18 @@
     return typeof object === "undefined";
   }
 
+  // Polyfill from https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Object/create
+  if (!Object.create) {
+    Object.create = function (o) {
+      if (arguments.length > 1) {
+        throw new Error('Object.create implementation only accepts the first parameter.');
+      }
+      function F() {}
+      F.prototype = o;
+      return new F();
+    };
+  }
+
   extend(Object, {
     type:          type,
     extend:        extend,
